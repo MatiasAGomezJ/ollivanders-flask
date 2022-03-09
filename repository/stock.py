1330fc31cc9086e0d7b544a297a6deb1a1120da7
MatiasAGomezJ <mgomez@cifpfbmoll.eu>
2022-03-09 08:15:15 +0100

    refactor: Añadir items a stock

--- repository/stock.py
+++ repository/stock.py
@@ -19,4 +19,24 @@ stock = [
         "quality": 6,
         "sell_in": 10,
     },
+    {
+        "name": "Aged_brie",
+        "quality": 10,
+        "sell_in": 15,
+    },
+    {
+        "name": "Sulfuras",
+        "quality": 50,
+        "sell_in": 7,
+    },
+    {
+        "name": "Aged_brie",
+        "quality": 22,
+        "sell_in": 8,
+    },
+    {
+        "name": "Conjured",
+        "quality": 12,
+        "sell_in": 5,
+    },
 ]
