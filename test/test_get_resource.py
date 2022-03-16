dbfa05f78e09a856ba7f0dd587757f4687eba51e
MatiasAGomezJ <mgomez@cifpfbmoll.eu>
2022-03-16 16:49:09 +0100

    test: :white_check_mark: Añadir test integración de un get

---
+++ test/test_get_resource.py
@@ -0,0 +1,18 @@
+import pytest
+import api
+import json
+
+@pytest.mark.get_resource
+def test_funciona():
+    app = api.app.test_client()
+    resp = app.get('/item?name=Sulfuras&quality=50&sell_in=7')
+
+    assert resp.status_code == 200
+
+    resp_dict = json.loads(resp.data.decode())[0]
+
+    assert resp_dict.get("name") == "Sulfuras"
+    assert resp_dict.get("quality") == 50
+    assert resp_dict.get("sell_in") == 7
+
+    assert resp.get_data() == b'[{"name": "Sulfuras", "quality": 50, "sell_in": 7}]\n'
