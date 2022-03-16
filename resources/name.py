76cf36439663ceb70381c523d443580ec263d54d
MatiasAGomezJ <mgomez@cifpfbmoll.eu>
2022-03-16 21:08:50 +0100

    feat: :sparkles: Añadir post en buscar por nombre

--- resources/name.py
+++ resources/name.py
@@ -11,3 +11,16 @@ class name(Resource):
             abort(404, message="No se han pasado los argumentos necesarios")
 
         return service.item_by_name(name)
+
+    def post(self):
+        args = request.args
+        name = args.get("name")
+        quality = 1
+        sell_in = 1
+
+        return [name, quality, sell_in]
+        if not name:
+            abort(404, message="No se han pasado los argumentos necesarios")
+
+        service.create_item(name, quality, sell_in)
+        return service.items(name, int(quality), int(sell_in)), 201
