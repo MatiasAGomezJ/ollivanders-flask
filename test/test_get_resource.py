7191af93845f0d8fe9c23ddea6e5deadea942fa8
MatiasAGomezJ <mgomez@cifpfbmoll.eu>
2022-06-17 19:20:17 +0200

    Crear test post

--- test/test_get_resource.py
+++ test/test_get_resource.py
@@ -2,8 +2,8 @@ import pytest
 import api
 import json
 
-@pytest.mark.get_resource
-def test_funciona():
+@pytest.mark.item_endpoint
+def test_get():
     app = api.app.test_client()
     resp = app.get('/item/Sulfuras/50/7')
 
@@ -16,3 +16,28 @@ def test_funciona():
     assert resp_dict.get("sell_in") == 7
 
     assert resp.get_data() == b'[{"name": "Sulfuras", "quality": 50, "sell_in": 7}]\n'
+
+
+def test_post_delete():
+    app = api.app.test_client()
+
+    name = "Time-Turner"
+    quality = 25
+    sell_in = 13
+    document = {
+        name: name,
+        quality: quality,
+        sell_in: sell_in
+    }
+
+    resp = app.post(f"/item/{name}/{quality}/{sell_in}")
+
+    assert resp.status_code == 200
+
+    resp_dict = json.loads(resp.data.decode())[0]
+
+    assert resp_dict.get("name") == name
+    assert resp_dict.get("quality") == quality
+    assert resp_dict.get("sell_in") == sell_in
+
+    assert resp.get_data(True) == '[{"name": "' + name + '", "quality": ' + str(quality) + ', "sell_in": ' + str(sell_in) + '}]\n'
