1dd9b992a25ed1327efab6f03fc29201d92048fd
MatiasAGomezJ <mgomez@cifpfbmoll.eu>
2022-06-17 19:28:38 +0200

    Actualizar get test

--- test/test_get_resource.py
+++ test/test_get_resource.py
@@ -5,17 +5,23 @@ import json
 @pytest.mark.item_endpoint
 def test_get():
     app = api.app.test_client()
-    resp = app.get('/item/Sulfuras/50/7')
+
+    name = "Sulfuras"
+    quality = 50
+    sell_in = 7
+
+    resp = app.get(f"/item/{name}/{quality}/{sell_in}")
 
     assert resp.status_code == 200
 
     resp_dict = json.loads(resp.data.decode())[0]
 
-    assert resp_dict.get("name") == "Sulfuras"
-    assert resp_dict.get("quality") == 50
-    assert resp_dict.get("sell_in") == 7
+    assert resp_dict.get("name") == name
+    assert resp_dict.get("quality") == quality
+    assert resp_dict.get("sell_in") == sell_in
 
-    assert resp.get_data(True) == '[{"name": "Sulfuras", "quality": 50, "sell_in": 7}]\n'
+    assert resp.get_data(True) == '[{"name": "' + name + '", "quality": ' + str(
+        quality) + ', "sell_in": ' + str(sell_in) + '}]\n'
 
 
 def test_post_delete():
