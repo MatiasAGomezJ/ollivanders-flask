5c0066e7874c219f0941139f4dfd2a625e9ccf4f
MatiasAGomezJ <mgomez@cifpfbmoll.eu>
2022-06-17 19:38:57 +0200

    Crear put test

--- test/test_get_resource.py
+++ test/test_get_resource.py
@@ -55,3 +55,44 @@ def test_post_delete():
 
     assert resp.get_data(True) != '[{"name": "' + name + '", "quality": ' + str(
         quality) + ', "sell_in": ' + str(sell_in) + '}]\n'
+
+
+def test_post_put():
+    app = api.app.test_client()
+
+    # Post
+
+    name = "Time-Turner"
+    quality = 25
+    sell_in = 13
+
+    resp = app.post(f"/item/{name}/{quality}/{sell_in}")
+
+    assert resp.status_code == 200
+
+    resp_dict = json.loads(resp.data.decode())[0]
+
+    assert resp_dict.get("name") == name
+    assert resp_dict.get("quality") == quality
+    assert resp_dict.get("sell_in") == sell_in
+
+    assert resp.get_data(True) == '[{"name": "' + name + '", "quality": ' + str(
+        quality) + ', "sell_in": ' + str(sell_in) + '}]\n'
+
+    new_name = "Wand"
+    new_quality = 3
+    new_sell_in = 5
+
+    resp = app.put(
+        f"/item/{name}/{quality}/{sell_in}/{new_name}/{new_quality}/{new_sell_in}")
+
+    assert resp.status_code == 200
+
+    resp_dict = json.loads(resp.data.decode())[0]
+
+    assert resp_dict.get("name") == new_name
+    assert resp_dict.get("quality") == new_quality
+    assert resp_dict.get("sell_in") == new_sell_in
+
+    assert resp.get_data(True) == '[{"name": "' + new_name + '", "quality": ' + str(
+        new_quality) + ', "sell_in": ' + str(new_sell_in) + '}]\n'
