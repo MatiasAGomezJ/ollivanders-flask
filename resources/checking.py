7b9ff8b44237f46c3217527fdfdf4c0e75bab857
MatiasAGomezJ <mgomez@cifpfbmoll.eu>
2022-06-17 17:52:25 +0200

    Crear funciones para quality y sell_in

--- resources/checking.py
+++ resources/checking.py
@@ -4,3 +4,13 @@ from flask_restful import abort
 def check_item_params(name, quality, sell_in):
     if not name or not quality.isnumeric() or not sell_in.isnumeric():
         abort(404, message="No se han pasado los argumentos correctos")
+
+
+def check_quality_param(quality):
+    if not quality.isnumeric():
+        abort(404, message="El parametro 'quality' tiene que ser un número")
+
+
+def check_sell_in_param(sell_in):
+    if not sell_in.isnumeric():
+        abort(404, message="El parametro 'sell_in' tiene que ser un número")
