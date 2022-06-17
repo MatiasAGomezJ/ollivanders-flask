0177aa6e41ec7c8d3830130149f51e32ccccba08
MatiasAGomezJ <mgomez@cifpfbmoll.eu>
2022-06-17 17:46:19 +0200

    Quitar post y quitar comprobación del parametro

--- resources/name.py
+++ resources/name.py
@@ -1,21 +1,7 @@
-from flask_restful import Resource, abort
+from flask_restful import Resource
 from services.service import service
 
 
 class name(Resource):
     def get(self, name):
-
-        if not name:
-            abort(404, message="No se han pasado los argumentos necesarios")
-
         return service.item_by_name(name)
-
-    def post(self, name):
-        quality = 1
-        sell_in = 1
-
-        if not name:
-            abort(404, message="No se han pasado los argumentos necesarios")
-
-        service.create_item(name, quality, sell_in)
-        return service.items(name, int(quality), int(sell_in)), 201
